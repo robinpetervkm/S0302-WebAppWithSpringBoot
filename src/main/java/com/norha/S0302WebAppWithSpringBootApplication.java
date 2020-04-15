dbe1952b8b1c48c5bae4be786c19233c9bbb4386
Robinclear <robin.peter.vkm@gmail.com>
2020-04-15 14:27:47 -0300

    SpringBootWeb App project using the spring boot starter project wizard with only springWeb project dependancy selected

---
+++ src/main/java/com/norha/S0302WebAppWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.norha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S0302WebAppWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S0302WebAppWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/norha/S0302WebAppWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S0302WebAppWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
