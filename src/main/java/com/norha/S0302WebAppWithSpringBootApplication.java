a5ccd4f8d5b31917493ef51054d05310cd802415
Robinclear <robin.peter.vkm@gmail.com>
2020-04-15 15:14:07 -0300

    Just Created the WebContoller Class annotation @Controller
    @RequestMapping("welcome") which return "welcome.html"
    
    created welcome.html in the static folder

--- src/main/java/com/norha/S0302WebAppWithSpringBootApplication.java
+++ src/main/java/com/norha/S0302WebAppWithSpringBootApplication.java
@@ -8,6 +8,11 @@ public class S0302WebAppWithSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(S0302WebAppWithSpringBootApplication.class, args);
+		//1 Loads the defaults
+		//2 Starts the boot application
+		//3 Performs class path scan
+		//4 Starts Local Tomcat 
+		
 	}
 
 }
---
+++ src/main/java/com/norha/WebController.java
@@ -0,0 +1,13 @@
+package com.norha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	@RequestMapping("welcome")
+	public String welcome() {
+		return "welcome.html";
+	}
+
+}
