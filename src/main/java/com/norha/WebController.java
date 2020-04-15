e0d3f1e1f2af6cd1f219d3dc0924d1492bbc8ce9
Robinclear <robin.peter.vkm@gmail.com>
2020-04-15 16:13:05 -0300

    just added the jsp file but when the url called the file get dowenloaded

--- src/main/java/com/norha/WebController.java
+++ src/main/java/com/norha/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
 	@RequestMapping("welcome")
 	public String welcome() {
-		return "welcome.html";
+		return "welcome.jsp ";
 	}
 
 }
